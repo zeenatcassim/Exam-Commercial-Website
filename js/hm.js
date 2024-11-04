04e299f37162e762a3867366d20afb3e842d73eb
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-11-04 07:44:56 +0200

    nav styling

---
+++ js/hm.js
@@ -0,0 +1,9 @@
+/*const aboutBtn = document.querySelector(".about-btn");
+
+if (designBtn) {
+    designBtn.addEventListener("click", function(){
+     window.location.href = "html/design.html"; })
+} */
+
+
+//loop through nav btns and go to next webpages
