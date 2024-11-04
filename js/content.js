3ed4e18691d4a0260988753ab5e7d3e19fffce5c
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-11-04 10:48:16 +0200

    data viz [1]

---
+++ js/content.js
@@ -0,0 +1,30 @@
+//This contains all the content that is added on the website through JavaScript
+
+//Style Guide
+
+export const styleGuide = [
+    {
+        title: 'Typography',
+        p: 'lorem ipsum',
+    },
+    {
+        title:'Visual Hierarchy and Styling Design Decisions' ,
+        paragraphs: [
+            'Using the feedback from Assignment 3, I changed the styling and user interface. For the homepage, the content is structured towards the centre but for the other webpages, the content is aligned from the left to the right. ',
+             'The feedback suggested that I centre the homepage content and when I tried the centre layout, I realised that it was much easier to read the headings and paragraphs compared to having it structured on the left. The aim of the homepage is to capture the user’s attention with brief sentences thus when the content is in the centre, it looks more attention grabbing instead of them being forced to read from the left corner to the right. ',
+             'I decided to keep the left alignment for the other webpages but only after I went to do some research. For my research, I looked at how current currency rate exchanges and forex trade websites, such as Oanda, Xe and Avatrade structured their content. They have important information that needs to be explained centred from the left but keep lots of white space between paragraphs and headings to rest the user’s eyes. Following the same principle, the structure I planned for the other webpages was to have a big heading, on the left followed by a brief paragraph with an image next to it. ',
+             'In the feedback, the problem of navigating through content heavy webpages was brought up. To solve this, I decided to have a small table of contents after the first heading so that instead of scrolling down, the user can click on what they want to read instantly. After they are done reading, they can click on the up arrow on the bottom right corner of the screen and go back up to look for something else. ', 
+            ],
+    }, {
+        title:'Test',
+        paragraphs: [
+            'Test 1',
+            'Test 2', 
+            'Test 2',
+            'Test 2',
+            'Test 2',
+            'Test 2',
+            'Test 2',
+        ]
+    }
+]
---
+++ js/design.js
@@ -0,0 +1,24 @@
+
+//Style Guide
+
+const styleGuideContainer = document.querySelector('.style-guide-container');
+
+import { styleGuide } from "./content.js";
+
+function populateStyleGuide(styleGuide){
+    styleGuide.forEach((sectionData, index) => {
+      const container = document.createElement('section');
+      container.classList.add('style-guide-section'); // Use a more specific class if needed
+  
+      container.innerHTML = `
+        <h3 class="style-guide-title p-title">${sectionData.title}</h3>
+      ${sectionData.paragraphs ? sectionData.paragraphs.map(p => `<p class= "style-guide-content p-summary"> ${p} </p>`).join('') : '<p> No Description Available.</p>'}
+   
+      `;
+    styleGuideContainer.appendChild(container);
+  }
+  )}
+
+populateStyleGuide(styleGuide);
+
+//${index === 1 ? `<img src="${sectionData.img || " alt="${sectionData.h3} image" class="style-guide-image" />` : ''}
